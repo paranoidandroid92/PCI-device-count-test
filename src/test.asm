e4bcb5192a1e406244303793a66ec36e65e8b7e3
Ali ATIL <alihaydar.atil@gmail.com>
2018-03-17 01:06:27 +0300

    source code

--- src/test.asm
+++ src/test.asm
@@ -4,10 +4,8 @@ mov ds,ax
 
 
 enumerate:
-	cmp byte [device_no],0x1F	; there could be 256 buses
+	cmp byte [device_no],0x20	; there could be 32 devices in each bus
 	je increment_bus_no 		
-	cmp byte [bus_no],0xFF		; there could be 32 devices in each bus
-	je end
 
 	;   31    30-24      23-16         15-11              10-8              7-2        1-0
 	; +---+----------+------------+---------------+-----------------+-----------------+----+
@@ -25,19 +23,25 @@ enumerate:
 	out dx,eax
 	mov dx,[CONFIG_DATA]
 	in eax,dx
+
+	add word [iteration_count],1
 	add byte [device_no],1
 	cmp eax,0xFFFFFFFF			; returns 0xFFFFFFFF for non-existing devices
 	je enumerate
-increment:
-	add byte [device_count],1
+	add word [device_count],1
 	jmp enumerate
 increment_bus_no:
+	cmp byte [bus_no],0xFF		; there could be 256 buses
+	je end
 	add byte [bus_no],1
 	mov byte [device_no],0
 	jmp enumerate
 
 end:
-	mov dword eax,[device_count]
+	xor eax,eax
+	mov word ax,[device_count]
+	xor ecx,ecx
+	mov word cx,[iteration_count]
 	hlt
 	jmp end
 
@@ -48,7 +52,9 @@ CONFIG_ADDRESS dw 0x0CF8		; PCI Address Port
 CONFIG_DATA dw 0x0CFC			; PCI Data Port
 bus_no db 0x00
 device_no db 0x00
-device_count db 0x00
+device_count dw 0x0000
+iteration_count dw 0x000000		; iteration count for debug purposes. 
+								; should be equal to 8192(0x2000) at the end of enumeration (256 * 32 = 8192)
 times 510-($-$$) db 0x00
 db 0x55
 db 0xAA
